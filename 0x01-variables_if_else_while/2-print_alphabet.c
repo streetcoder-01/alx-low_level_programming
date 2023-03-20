33ca748aac072d4b02faaee5168010d695ab084d
streetcoder-01 <125763501+streetcoder-01@users.noreply.github.com>
2023-03-20 10:22:30 +0100

    Control flow in C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
